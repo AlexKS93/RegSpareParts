b3f71b58aa5cc90212d067ce263b7f95cde4df39
AlexKS93 <Alex-ska93@mail.ru>
2023-07-11 16:10:42 +0300

    Получение столбцов и строк с сервера

--- backend/api/views.py
+++ backend/api/views.py
@@ -9,5 +9,12 @@ def get_fields_list(request):
     fields_name = [field.name for field in SpareParts._meta.get_fields()]
     fields_list = dict(zip(fields_name, fields_verbose_name))
     print(fields_list)
-
-    return JsonResponse([{'title': 'state', 'field': 'state',}], safe=False)#fields_list)
+    output_field = []
+    for index, item in enumerate(fields_list.keys()):
+        if index == 0:
+            output_field.append({'title': 'status',
+                                 'field': 'status',
+                                 'checkbox': 'true'})
+            continue
+        output_field.append({'title': fields_list[item], 'field': item})
+    return JsonResponse([output_field], safe=False)
